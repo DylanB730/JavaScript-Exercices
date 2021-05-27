01c02ba1c52bdf4d6efdcffb77a906ff29c000d3
DylanB730 <dylan.bonafos@gmail.com>
2021-05-27 21:40:29 +0200

    Projet validant THP JavaScript

---
+++ script_2.js
@@ -0,0 +1,11 @@
+// Factiorial 
+/*
+let nb = prompt("De quel nombre veux-tu calculer la factorielle ?");
+function factoriel(x){
+    if(x<0) throw new Error("factoriel n'est définie que pour les entiers positifs");
+    if(x<=1)return 1;
+    else return x*factoriel(x-1);
+}
+console.log("Le resultat est : " + nb*factoriel(nb-1));
+*/
+
---
+++ script_3.js
@@ -0,0 +1,8 @@
+/*
+let nb = prompt("Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?");
+
+for( count = 1; count <= nb; count++)
+{
+  console.log(" ".repeat(nb-count)+"#".repeat(count))
+}
+*/
